ab3b509478e12ea6c4d14b8a7a60065f84699831
danielchochlinski <danielchochlinski@hotmail.com>
2022-12-11 20:27:10 +0100

    text changes

---
+++ pages/api/contactForm.js
@@ -0,0 +1,33 @@
+/* eslint-disable import/no-anonymous-default-export */
+const mail = require("@sendgrid/mail");
+const key = process.env.SENDGRID_API_KEY;
+
+mail.setApiKey(key);
+
+export default async (req, res) => {
+  try {
+    console.log(req.body);
+    const body = JSON.parse(req.body);
+    console.log(body);
+    const message = `
+    Name: ${body.fullName}<br/>
+    Email: ${body.emailAddress}<br/>
+    PhoneNumber: ${body.phoneNumber}<br/>
+    Message: ${body.message}<br/>`;
+
+    const data = {
+      to: "logistics.krk.2022@gmail.com",
+      from: "gabinet@emgneurologmalopolska.com",
+      subject: "TRANSCOMLOGISTICS Message",
+      test: message,
+      html: message,
+    };
+
+    await mail.send(data);
+
+    res.status(200).json({ status: "Ok" });
+  } catch (error) {
+    console.log("ERROR", error);
+    res.status(400).send("Message not sent.");
+  }
+};
--- views/offer/Offer.jsx
+++ views/offer/Offer.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import styles from "./offer.module.scss";
 import Image from "next/image";
@@ -20,18 +21,42 @@ const Offer = () => {
         <img src="./land.svg" alt="land" />
         <h3>{t.road_header}</h3>
         <p>{t.road_p}</p>
+        <span>{t.road_h}</span>
+        <div className={styles.bullets}>
+          <span>{t.road_one}</span>
+          <span>{t.road_two}</span>
+          <span>{t.road_three}</span>
+          <span>{t.road_four}</span>
+        </div>
+        <p>{t.road_p_two}</p>
       </div>
 
       <div className={styles.offer_box}>
         <img src="./sea.svg" alt="sea" />
         <h3>{t.naval_header}</h3>
         <p>{t.naval_p}</p>
+        <div className={styles.naval_box}>
+          <span>{t.naval_q_one}</span>
+          <span>{t.naval_a_one}</span>
+        </div>
+        <div className={styles.naval_box}>
+          <span>{t.naval_q_two}</span>
+          <span>{t.naval_a_two}</span>
+        </div>
       </div>
 
       <div className={styles.offer_box}>
         <img src="./air.svg" alt="air" />
         <h3>{t.air_header}</h3>
         <p>{t.air_p}</p>
+        <div className={styles.naval_box}>
+          <span>{t.naval_q_one}</span>
+          <span>{t.naval_a_one}</span>
+        </div>
+        <div className={styles.naval_box}>
+          <span>{t.air_q_two}</span>
+          <span>{t.air_a_two}</span>
+        </div>
       </div>
     </div>
   );
